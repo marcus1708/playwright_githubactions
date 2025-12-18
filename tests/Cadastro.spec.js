c2590f663f5a9a07029b8f4055f8aa68869fd8af
marcus1708 <marcusviniciusbordin@gmail.com>
2025-12-18 13:35:39 -0300

    correção

--- tests/Cadastro.spec.js
+++ tests/Cadastro.spec.js
@@ -1,21 +1,25 @@
-const {test} = require('@playwright/test');
-const {Login,Cadastro} = require('../pages/Serverest');
+const { test } = require('@playwright/test');
+const { Login, Cadastro } = require('../pages/Serverest');
 
-const nome = 'QA';
-const email = 'qa@qa.io';
-const senha = '123';
+test.describe('Cadastro com Sucesso', () => {
+  test('Realiza Cadastro com Sucesso', async ({ page }) => {
+    const login = new Login(page);
+    const cadastro = new Cadastro(page);
 
-test.describe('Cadastro com Sucesso',() =>{
-    test('Realiza Cadsatro com Sucesso', async ({page}) =>{
-        const login = new Login(page);
-        const cadastro = new Cadastro(page);
-       
-        // 1. Acessar a pagina de Login
-        await login.goto();
-        await cadastro.Cadastrar();
-        // 2. Preencher os dados de cadastro
-        await cadastro.Fazer_Cadastro(nome,email, senha);
-        // 3. Verificar se o cadastro foi realizado com sucesso
-        await cadastro.Verif_Home();
-    });
+    const nome = 'QA Teste';
+    const email = `qa${Date.now()}@qa.com`; // evita usuário duplicado
+    const senha = '123456';
+
+    // 1. Acessar a página de Login
+    await login.goto();
+
+    // 2. Abrir tela de cadastro
+    await cadastro.Abrir_Tela_Cadastro();
+
+    // 3. Preencher os dados de cadastro
+    await cadastro.Fazer_Cadastro(nome, email, senha);
+
+    // 4. Verificar se o cadastro foi realizado com sucesso
+    await cadastro.Verif_Cadastro_Com_Sucesso();
+  });
 });
