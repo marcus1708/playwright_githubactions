d4d826754eaa61286a3c6b2ae0dab01ddab7501c
marcus1708 <marcusviniciusbordin@gmail.com>
2025-12-18 13:39:11 -0300

    correção

--- tests/Usuario.spec.js
+++ tests/Usuario.spec.js
@@ -1,19 +1,24 @@
-const {test} = require('@playwright/test');
-const {Login,Cadastro,Usuario} = require('../pages/Serverest');
+const { test } = require('@playwright/test');
+const { Login, Usuario } = require('../pages/Serverest');
 
 const email = 'qa@qa.io';
 const senha = '123';
 
-test.describe('Listar usuario',() =>{
-    test('Lista usuario cadastr.', async ({page}) =>{
-        const login = new Login(page);
-        const usuario = new Usuario(page);
-        const cadastro = new Cadastro(page);
-        
-        // 1. Acessar a pagina de Login
-        await login.goto();
-        await login.Fazer_Login(email, senha);
-        await cadastro.Verif_Home();
-        await usuario.Listar_Usuario();
-    });
+test.describe('Listar usuario', () => {
+  test('Lista usuario cadastr.', async ({ page }) => {
+    const login = new Login(page);
+    const usuario = new Usuario(page);
+
+    // 1. Acessar a página de Login
+    await login.goto();
+
+    // 2. Realizar login
+    await login.Fazer_Login(email, senha);
+
+    // 3. Validar que está na Home
+    await login.Verif_Home();
+
+    // 4. Listar usuários
+    await usuario.Listar_Usuario();
+  });
 });
